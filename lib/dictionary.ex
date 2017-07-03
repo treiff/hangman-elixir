c80e9220d5129e8cc5611dfdef8e69231ab41344
Trevor Reiff <trevorreiff@gmail.com>
2017-07-03 12:03:46 -0400

    Add assets directory and words dictionary file.

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -1,18 +1,5 @@
 defmodule Dictionary do
-  @moduledoc """
-  Documentation for Dictionary.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Dictionary.hello
-      :world
-
-  """
   def hello do
-    :world
+    IO.puts "Hello there"
   end
 end
